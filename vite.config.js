01baf4e2f8088498b562ded42c49ba894f3197a7
jossmarsala <marsalahjosefina@gmail.com>
2026-04-25 23:24:47 -0300

    Add: Icon for direct access.

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      includeAssets: ['favicon.svg', 'apple-touch-icon.png', 'masked-icon.svg'],
       manifest: {
         name: 'Misu — OS',
         short_name: 'Misu',
